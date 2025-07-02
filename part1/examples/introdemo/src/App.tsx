f2fad7aa99de8dfebc30b07618edbca3dbab4c30
Tomi <59865056+andreasvartiainen@users.noreply.github.com>
2025-07-02 21:50:42 +0300

    example stuff

--- part1/examples/introdemo/src/App.tsx
+++ part1/examples/introdemo/src/App.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import './App.css'
 
+const Button = (props: {text: string, onClick: () => void}) => (
+	<button onClick={props.onClick}>
+	{ props.text }
+	</button>
+)
+
 const App = () => {
+	const [value, setValue] = useState<number>(0);
+
 	const handleClick = (who: string) => () => {
 		console.log(`hello ${who}`)
 	};
 
+	const addValue = (amount: number) => () => {
+		setValue(value + amount);
+	}
+
+
 	return (
 		<div>
-		<button onClick={handleClick("me")}>BUTTON</button>
+		{value}
+		<Button text="hello" onClick={handleClick("tomi")}/>
+		<Button text="add" onClick={addValue(100)}/>
 		</div>
 	)
 }
