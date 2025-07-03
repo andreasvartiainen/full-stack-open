cf5516a772ff58fc965668a033551e86256802e3
Tomi <59865056+andreasvartiainen@users.noreply.github.com>
2025-07-03 15:51:21 +0300

    2.6 phonebook step 1

--- examples/part2-example/src/App.jsx
+++ examples/part2-example/src/App.jsx
@@ -1,18 +1,52 @@
+import { useState } from "react";
 import Note from "./components/note";
 
-const Example = (props) => {
-	console.log("props: ", props);
-}
+const FILTER_LIST = [
+	(note) => note.important,
+	() => true,
+];
 
-function App({ notes }) {
+function App(props) {
+	const [notes, setNotes] = useState(props.notes);
+	const [newNote, setNewNote] = useState('');
+	const [showAll, setShowAll] = useState(true);
 
-	const noteList = notes.map(note => <Note note={note}/>);
+	// unary + turns boolean to 1 or 0
+	const noteList = notes.filter(FILTER_LIST[+showAll]).map(note => <Note key={note.id} note={note}/>);
+
+	const addNote = (event) => {
+		event.preventDefault();
+		console.log('button clicked' , event.target);
+		const noteObject =  {
+			content: newNote,
+			important: Math.random() < 0.5,
+			id: String(notes.length + 1),
+		};
+		setNotes([...notes, noteObject]);
+		setNewNote('');
+	}
+
+	const handleNoteChange = (event) => {
+		console.log(event.target.value);
+		setNewNote(event.target.value);
+	}
+
+	// const notesToShow = showAll
+	// 	? notes
+	// 	: notes.filter(note => note.important)
 
   return (
 		<>
 		<h1>Notes</h1>
 		<ul>
 		 {noteList}
+		<form onSubmit={addNote}>
+			<input placeholder="new note" value={newNote} onChange={handleNoteChange}/>
+			<button type="submit">save</button>
+		</form>
+		<button onClick={() => setShowAll(!showAll)}>
+		{showAll ? 'important' : 'all'}
+		</button>
 		</ul>
 		</>
   )
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+	const handleChange = (event) => {
+		console.log(newName);
+		setNewName(event.target.value);
+	}
+
+	const addPerson = (event) => {
+		event.preventDefault();
+		const newPerson = {
+			name: newName,
+		};
+
+		setPersons([...persons, newPerson]);
+		setNewName('');
+	}
+
+	const listPersons = persons.map((person) => <div key={person.name}>{person.name}</div> )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input onChange={handleChange}/>
+        </div>
+        <div>
+          <button type="submit" onClick={addPerson}>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+			{listPersons}
+    </div>
+  )
+}
+
+export default App
