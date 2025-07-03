035cf2a0866b7beb470b6657a4ff53a300d8572f
Tomi <59865056+andreasvartiainen@users.noreply.github.com>
2025-07-03 16:28:54 +0300

    2.9 phonebook step 4 ready add filtering

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 
+const Input = ({text, value, onChange}) => {
+	return (
+		<div>
+			{text}: <input value={value} onChange={onChange}/>
+		</div>
+	)
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '358 505 050'}
+    { name: 'Arto Hellas', phone: '358 505 050', id: 0},
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [filter, setFilter] = useState('')
 
 	const handleName = (event) => {
 		console.log(newName);
@@ -36,6 +48,7 @@ const App = () => {
 		const newPerson = {
 			name: newName,
 			phone: newPhone,
+			id: persons.length + 1
 		};
 
 		setPersons([...persons, newPerson]);
@@ -43,18 +56,30 @@ const App = () => {
 		setNewPhone('');
 	}
 
-	const listPersons = persons.map((person) => <div key={person.name}>{person.name} {person.phone}</div> )
+	const handleFilter = (event) => {
+		setFilter(event.target.value);
+	}
+
+	// filter for getting substring from an item
+	const f = (person) => {
+		console.log(person.name.indexOf(filter));
+		// put filter and name to lowercase
+		return person.name.toLowerCase().indexOf(filter.toLowerCase()) != -1
+	}
+
+	// filter and add the items to the list use id as key
+	const listPersons = persons.filter(f).map((person) => 
+		<div key={person.id}>{person.name} {person.phone}</div> 
+	)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Input text="filter shown with" value={filter} onChange={handleFilter}/>
+      <h2>Add a new</h2>
       <form>
-        <div>
-          name: <input value={newName} onChange={handleName}/>
-        </div>
-				<div>
-          number: <input value={newPhone} onChange={handlePhone}/>
-				</div>
+        <Input text="name" value={newName} onChange={handleName}/>
+				<Input text="number" value={newPhone} onChange={handlePhone}/>
         <div>
           <button type="submit" onClick={addPerson}>add</button>
         </div>
