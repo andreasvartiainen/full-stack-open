7c748752aecd9235c46d772d40d6d938901f2ec0
Tomi <59865056+andreasvartiainen@users.noreply.github.com>
2025-07-09 20:54:29 +0300

    linting added

--- part2/phonebook/src/services/services.js
+++ part2/phonebook/src/services/services.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
-const baseUrl = '/api/persons'
+const baseUrl = '/api/persons';
 
 const getAll = async () => {
-	const request = axios.get(baseUrl)
+	const request = axios.get(baseUrl);
 	return request.then((response) => response.data);
-}
+};
 
 const create = async (newObject) => {
 	const request = axios.post(baseUrl, newObject);
 	return request.then(response => response.data);
-}
+};
 
 const update = async (id, newObject) => {
 	const request = axios.put(`${baseUrl}/${id}`, newObject);
 	return request.then(response => response.data);
-}
+};
 
 // remove item from the database
 const remove = async (id) => {
@@ -23,6 +23,6 @@ const remove = async (id) => {
 	return request.then((response) => {
 		return response.data;
 	});
-}
+};
 
 export default {getAll, create, update, remove};
